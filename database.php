9bcd14a1835c8b795a644afaf51e2ca4e736173b
Isabel An <isabel.an@wustl.edu>
2020-10-06 16:42:19 -0500

    updated database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli('localhost', 'username', 'password', 'databasename');
+$mysqli = new mysqli('localhost', 'module3', 'isabelandclaire', 'news_site');
 
 if($mysqli->connect_errno) {
 	printf("Connection Failed: %s\n", $mysqli->connect_error);

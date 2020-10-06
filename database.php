838182d1674d49a35558f0ebd330a026c4c3989c
Isabel An <isabel.an@wustl.edu>
2020-10-06 01:22:15 -0500

    set up

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+
+$mysqli = new mysqli('localhost', 'username', 'password', 'databasename');
+
+if($mysqli->connect_errno) {
+	printf("Connection Failed: %s\n", $mysqli->connect_error);
+	exit;
+}
+?>
